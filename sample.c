cf7f6a0a5b48e355617b683fbe9b4e13a6dac765
Soniq Nagra <soniq.nagra@uon.edu.au>
2019-04-29 05:00:06 +0000

    efiug

---
+++ sample.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char* text = "qwertyuiopasdfghjklzxcvbnm";    // cipher lookup
+
+    char input[500];                                    // input buffer
+    printf("Enter text: ");
+    fgets(input, sizeof(input), stdin);                 // safe input from user
+    input[strlen(input) - 1] = 0;                       // remove the \n (newline)
+    int count = strlen(input);                          // get the string length
+
+    char output[count];                                 // output string
+    for(int i = 0; i < count; i++) {                    // loop through characters in input
+        int index = ((int) input[i]) - 97;              // get the index in the cipher by subtracting 'a' (97) from the current character
+        if(index < 0) {
+            output[i] = ' ';                            // if index < 0, put a space to account for spaces
+        }
+        else {
+            output[i] = ciphertext[index];              // else, assign the output[i] to the ciphertext[index]
+        }
+    }
+    output[count] = 0;                                  // null-terminate the string
+
+    printf("output: %s\n", output);                     // output the result
+}
